e5627e26acf37de7ed442860ca24b7961ba6cdfd
Caio Assis <caio.ab.assis@gmail.com>
2024-05-05 17:39:02 -0300

    Altera updateUser

--- src/components/modal/funcionario/user-list/index.tsx
+++ src/components/modal/funcionario/user-list/index.tsx
@@ -1,18 +1,38 @@
-import { Box, Center, Text } from "@chakra-ui/react"
+import { Box, Button, Text } from "@chakra-ui/react"
 import { Funcionario } from "../../../interfaces/funcionario"
+import CreateModal from "../../create-modal"
+import { useState } from "react"
 
 interface UserProps {
     user: Funcionario
 }
-
+////////////NAO FUNCIONA AINDA
 export default function UserList({ user }: UserProps) {
+
+    function EditarTarefa(){
+        const [updateOpen, setUpdateOpen] = useState(true)
+        const handleOpenModalUpdate = () => {
+            setUpdateOpen(true)
+        }
+    
+        const handleCloseModalUpdate = () => {
+            setUpdateOpen(false)
+        }
+        return(
+            <CreateModal label='Editar Usuário' isOpen={updateOpen} onClose={handleCloseModalUpdate}>
+                yes
+            </CreateModal>
+        )
+    }
+
     return (
-        <Box bg='lightgray' w='50%' p={2} color='black' display='flex' borderRadius='lg' gap={5} margin='5px'>
-            <Text>
-                <Center>
+        <Box w='80%' p={2}display='flex'gap={5} margin='5px'>
+            <Button w='100%' bg='lightgray' justifyContent='flex-start'
+            onClick={EditarTarefa}>
+                <Text alignContent=''>
                     {user.matricula} - {user.nome}
-                </Center>
-            </Text>
+                </Text>
+            </Button>
         </Box>
     )
 }
