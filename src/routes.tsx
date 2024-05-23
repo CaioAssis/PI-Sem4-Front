60497593ab69bf99a1b74c7bac5873e008dd0f65
Caio Assis <caio.ab.assis@gmail.com>
2024-05-23 01:06:54 -0300

    cria paginas vistoria

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,16 @@ import Sobre from './pages/sobre'
 import NotFound from './pages/404'
 import AdmUser from './pages/admUser'
 import AdmClient from './pages/admClient'
+import Vist from './pages/vist'
+import VistCreate from './pages/vist-create'
 
 const routes = createBrowserRouter([
-  {path: '/', errorElement: <NotFound/>},
-  {path: '/sobre', element:<Sobre />},
-  {path: '/admUser', element:<AdmUser />},
-  {path: '/admClient', element:<AdmClient />}
+  { path: '/', errorElement: <NotFound /> },
+  { path: '/sobre', element: <Sobre /> },
+  { path: '/admUser', element: <AdmUser /> },
+  { path: '/admClient', element: <AdmClient /> },
+  { path: '/vistoria', element: <Vist /> },
+  { path: '/initVistoria', element: <VistCreate /> }
 ])
 
 export default routes //prof usou router
