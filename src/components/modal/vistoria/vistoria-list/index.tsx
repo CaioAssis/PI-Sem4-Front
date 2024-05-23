efa3fd4af55c673495f46bc11d9b6959bc7032eb
CaioAssis <caio.ab.assis@gmail.com>
2024-05-23 18:29:33 -0300

    adiciona modal vistoria

--- src/components/modal/vistoria/vistoria-list/index.tsx
+++ src/components/modal/vistoria/vistoria-list/index.tsx
@@ -1,22 +1,45 @@
 import { Box, Button, Text } from "@chakra-ui/react"
-import CreateModal from "../../create-modal"
+import CreateModal from "../../create-modal/index"
 import { useState } from "react"
 import { Vistoria } from "../../../interfaces/vistoria"
-import { useNavigate } from "react-router-dom"
+import ModalButton from "../../modal-button/index"
+import VistoriaView from "../vistoria-view/index"
 
 interface UserProps {
     vistoria: Vistoria
 }
 export default function VistoriaList({ vistoria }: UserProps) {
-    const navigate = useNavigate()
+
+
+const [createOpen, setCreateOpen] = useState(false)
+const [updateOpen, setUpdateOpen] = useState(false)
+const [updateShow, setUpdateShow] = useState(false)
+const handleOpenModalCreate = () => {
+    setCreateOpen(true)
+}
+
+const handleCloseModalCreate = () => {
+    setCreateOpen(false)
+}
+
+const handleOpenModalUpdate = () => {
+    setUpdateOpen(true)
+}
+
+const handleCloseModalUpdate = () => {
+    setUpdateOpen(false)
+}
+
+const handleUpdateShow = () => {
+    setUpdateShow(!updateShow);
+  };
     return (
         <Box w='80%' p={2} display='flex' gap={5} margin='5px'>
-            <Button w='100%' bg='lightgray' justifyContent='flex-start'
-                onClick={() => {navigate('/sobre')}}>
-                <Text alignContent=''>
-                    {vistoria.data}
-                </Text>
-            </Button>
+            <ModalButton label={vistoria.data}
+                onClick={ handleOpenModalCreate } />
+            <CreateModal label='PDF' isOpen={createOpen} onClose={handleCloseModalCreate}>
+                <VistoriaView vistoria={vistoria} onClose={handleCloseModalCreate} />
+            </CreateModal>
         </Box>
     )
 }
