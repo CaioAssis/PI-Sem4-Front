a42f4096e911e308c0d2293fdbdc8da3dd108d46
Bruno-William-0 <bruwill04@gmail.com>
2024-05-23 21:24:19 -0300

    Login

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Flex } from "@chakra-ui/react"
+import { Grid, GridItem, Flex, Box } from "@chakra-ui/react"
 import  SimpleSidebar  from "../sidebar/index"
 interface Props {
     children: React.ReactNode
@@ -8,20 +8,9 @@ function Layout(props: Props) {
     const { children } = props
     return (
         <SimpleSidebar>
-        <Grid className='grid' templateColumns='5fr' templateRows='auto'minHeight={'100vh'} backgroundSize={"cover"} backgroundRepeat="no-repeat" backgroundImage="url(https://www.deere.com.br/assets/images/region-3/products/harvesters/s-series/s760/r3g002593_colheitadeira_s760_campo_1_large_f312bb1da719e1a7196373ce1c8c6b7bd3cc165e.jpg)">
-      
-          <GridItem colSpan={2} rowSpan={3}>
-
-
-          </GridItem>
-          <GridItem colSpan={1} rowSpan={1}>
+        <Box minHeight={'100vh'} backgroundSize={"cover"} backgroundRepeat="no-repeat" backgroundImage="url(https://www.deere.com.br/assets/images/region-3/products/harvesters/s-series/s760/r3g002593_colheitadeira_s760_campo_1_large_f312bb1da719e1a7196373ce1c8c6b7bd3cc165e.jpg)">
               {children}
-          </GridItem>
-
-          <GridItem colSpan={1} rowSpan={1}>
-              
-          </GridItem> 
-        </Grid>
+        </Box>
         </SimpleSidebar>
     )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,15 @@ import Sobre from './pages/sobre'
 import NotFound from './pages/404'
 import AdmUser from './pages/admUser'
 import AdmClient from './pages/admClient'
+import Login from './pages/login'
 
 const routes = createBrowserRouter([
   {path: '/', errorElement: <NotFound/>},
   {path: '/sobre', element:<Sobre />},
   {path: '/admUser', element:<AdmUser />},
-  {path: '/admClient', element:<AdmClient />}
+  {path: '/admClient', element:<AdmClient />},
+  {path:'/Login', element: <Login />}
+
 ])
 
 export default routes //prof usou router
