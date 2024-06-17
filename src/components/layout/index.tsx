b014af0ea71dde709f0d01422c735b277e868677
Caio Assis <caio.ab.assis@gmail.com>
2024-06-17 19:40:20 -0300

    altera width

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -21,15 +21,15 @@ function Layout(props: Props) {
                 backgroundSize={"cover"}
                 backgroundRepeat="no-repeat"
                 minHeight="100vh"
-                minWidth="100vh"
+                minWidth="100vw"
                 backgroundImage="url(https://www.deere.com.br/assets/images/region-3/products/harvesters/s-series/s760/r3g002593_colheitadeira_s760_campo_1_large_f312bb1da719e1a7196373ce1c8c6b7bd3cc165e.jpg)"
             >
                 <Box
                     display={isHomePage ? 'none': 'block'}
                     maxHeight="80vh"
                     minHeight="80vh"
-                    maxWidth="100vh"
-                    minWidth="100vh"
+                    maxWidth="100vw"
+                    minWidth="100vw"
                     overflow="auto"
                     bg="rgba(0, 0, 0, 0.3)"
                     boxShadow={'0px 0px 7px 1px rgba(0,0,0,0.75)'}
