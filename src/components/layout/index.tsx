e49244f25a0d3328fc892d19a96a09e6079900a4
Caio Assis <caio.ab.assis@gmail.com>
2024-06-18 17:05:46 -0300

    Altera layout para vmin

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -20,16 +20,16 @@ function Layout(props: Props) {
                 justifyContent="center"
                 backgroundSize={"cover"}
                 backgroundRepeat="no-repeat"
-                minHeight="100vh"
-                minWidth="100vw"
+                minHeight="100vmin"
+                minWidth="100vmin"
                 backgroundImage="url(https://www.deere.com.br/assets/images/region-3/products/harvesters/s-series/s760/r3g002593_colheitadeira_s760_campo_1_large_f312bb1da719e1a7196373ce1c8c6b7bd3cc165e.jpg)"
             >
                 <Box
                     display={isHomePage ? 'none': 'block'}
-                    maxHeight="80vh"
-                    minHeight="80vh"
-                    maxWidth="100vw"
-                    minWidth="100vw"
+                    maxHeight="80vmin"
+                    minHeight="80vmin"
+                    maxWidth="90vmin"
+                    minWidth="90vmin"
                     overflow="auto"
                     bg="rgba(0, 0, 0, 0.3)"
                     boxShadow={'0px 0px 7px 1px rgba(0,0,0,0.75)'}
