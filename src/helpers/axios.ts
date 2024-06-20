2559be7b2ff16da7a8a74d05bb0585bacd19263c
Caio Assis <caio.ab.assis@gmail.com>
2024-06-20 12:28:53 -0300

    Modifica helper

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    //baseURL: 'https://pi-sem4.onrender.com/'
-    baseURL: 'http://localhost:3000/'
+    baseURL: 'https://pi-sem4.onrender.com/'
+    //baseURL: 'http://localhost:3000/'
 
 })
 
